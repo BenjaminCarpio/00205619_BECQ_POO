234e45083e1e00741fed547e09818fdf1c5a9704
BenjaminCarpio <00205619@uca.edu.sv>
2020-04-22 18:01:56 -0600

    Add: Labo 5 ejercicio 1

---
+++ Laboratorios POO/Laboratorio 05/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numero = 0, fibo1 = 0, fibo2 = 1, aux = 0;
+            Console.WriteLine("\n\tSeries de fibonacci");
+            Console.Write("Indique hasta que numero desea realizar la serie de fibonacci:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write(fibo1 + "\t");
+                aux = fibo1 + fibo2;
+                fibo1 = fibo2;
+                fibo2 = aux;
+            }
+        }
+    }
+}
