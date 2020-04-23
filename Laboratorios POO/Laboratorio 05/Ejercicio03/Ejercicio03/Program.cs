6e00590ff2df6425f6ecb75fd00a1d3320d8c450
BenjaminCarpio <00205619@uca.edu.sv>
2020-04-22 19:17:51 -0600

    Add: labo 05 Ejercicio 03 & Ejercicio 04

---
+++ Laboratorios POO/Laboratorio 05/Ejercicio03/Ejercicio03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Indique hasta que numero se realizara la recursion:  ");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            int i = 1;  //contador
+            Recursive(n, x, i);
+        }
+        
+        static void Recursive(int n, int x, int i) {
+            if (i < n && x < n) {
+                Console.Write(x + "  ");
+                x++;
+                i++;
+                Recursive(n, x, i);
+            }
+            else if(x==n){
+                Console.Write(x + "  ");
+                x--;
+                Recursive(n,x,i);
+                i++;
+            }
+            else if(x < n && x > 0){
+                Console.Write(x + "   ");
+                x--;
+                Recursive(n,x,i);
+            }
+            else if(x==0)
+                return;
+        }
+    }
+}
---
+++ Laboratorios POO/Laboratorio 05/Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese una frase para realizar los cambios:\t");
+            string frase = Console.ReadLine();
+            string newFrase = frase.Replace('a' , 'b');
+            frase = newFrase.Replace('A', 'B');
+            //2 veces para tomar en cuenta mayusculas y  minusculas
+            Console.WriteLine(frase);
+        }
+    }
+}
