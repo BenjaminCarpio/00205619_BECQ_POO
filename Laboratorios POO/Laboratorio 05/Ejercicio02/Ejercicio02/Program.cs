2c7e67da406f0a7b89d70ae82fd201aa14efaed3
BenjaminCarpio <00205619@uca.edu.sv>
2020-04-22 18:28:56 -0600

    Add: Labo 05 Ejercicio 02

---
+++ Laboratorios POO/Laboratorio 05/Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Ejercicio02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tVerificador de palindromos");
+            if (palindromo()) {
+                Console.WriteLine("Si es palindromo!");
+            }else{
+                Console.WriteLine("No es palindromo!");
+            }
+        }
+
+        static bool palindromo()
+        {
+            Console.WriteLine("Ingrese una palabra:");
+            String frase = Console.ReadLine();
+            bool EsONoEs = true;
+            int fin = frase.Length - 1;
+            int ini = 0;
+            int tamano = frase.Length;
+            for (int i = 0; i < tamano; i++)
+            {
+                if (frase[i] != frase[tamano - i - 1])
+                {
+                    EsONoEs = false;
+                }
+            }
+            return EsONoEs;
+        }
+    }
+}
