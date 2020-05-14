e9a9c347ba2a9c2eced28de7f56572e4c694a76c
BenjaminCarpio <00205619@uca.edu.sv>
2020-05-14 11:47:09 -0600

    Add: Labo 09

---
+++ Laboratorios POO/Laboratorio 09/Ejercicio01/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Ejercicio01
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
