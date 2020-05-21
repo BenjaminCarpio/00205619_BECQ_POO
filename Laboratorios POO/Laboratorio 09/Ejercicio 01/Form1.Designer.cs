10ed426421977ae68766c304afed603a03ca0b57
BenjaminCarpio <00205619@uca.edu.sv>
2020-05-21 17:05:01 -0600

    Add: Labo 9

---
+++ Laboratorios POO/Laboratorio 09/Ejercicio 01/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Ejercicio_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnConsult = new System.Windows.Forms.Button();
+            this.btnInscription = new System.Windows.Forms.Button();
+            this.btnSignature = new System.Windows.Forms.Button();
+            this.btnStudent = new System.Windows.Forms.Button();
+            this.registerStudent1 = new Ejercicio_01.RegisterStudent();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(
+                new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.btnConsult, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnInscription, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSignature, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnStudent, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.registerStudent1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(
+                new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(623, 508);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnConsult
+            // 
+            this.btnConsult.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnConsult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnConsult.Location = new System.Drawing.Point(468, 3);
+            this.btnConsult.Name = "btnConsult";
+            this.btnConsult.Size = new System.Drawing.Size(152, 44);
+            this.btnConsult.TabIndex = 3;
+            this.btnConsult.Text = "Consultar";
+            this.btnConsult.UseVisualStyleBackColor = true;
+            this.btnConsult.Click += new System.EventHandler(this.btnConsult_Click);
+            // 
+            // btnInscription
+            // 
+            this.btnInscription.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnInscription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnInscription.Location = new System.Drawing.Point(313, 3);
+            this.btnInscription.Name = "btnInscription";
+            this.btnInscription.Size = new System.Drawing.Size(149, 44);
+            this.btnInscription.TabIndex = 2;
+            this.btnInscription.Text = "Inscripcion";
+            this.btnInscription.UseVisualStyleBackColor = true;
+            this.btnInscription.Click += new System.EventHandler(this.btnInscription_Click);
+            // 
+            // btnSignature
+            // 
+            this.btnSignature.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSignature.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSignature.Location = new System.Drawing.Point(158, 3);
+            this.btnSignature.Name = "btnSignature";
+            this.btnSignature.Size = new System.Drawing.Size(149, 44);
+            this.btnSignature.TabIndex = 1;
+            this.btnSignature.Text = "Materia";
+            this.btnSignature.UseVisualStyleBackColor = true;
+            this.btnSignature.Click += new System.EventHandler(this.btnSignature_Click);
+            // 
+            // btnStudent
+            // 
+            this.btnStudent.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnStudent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStudent.Location = new System.Drawing.Point(3, 3);
+            this.btnStudent.Name = "btnStudent";
+            this.btnStudent.Size = new System.Drawing.Size(149, 44);
+            this.btnStudent.TabIndex = 0;
+            this.btnStudent.Text = "Estudiante";
+            this.btnStudent.UseVisualStyleBackColor = true;
+            this.btnStudent.Click += new System.EventHandler(this.btnStudent_Click);
+            // 
+            // registerStudent1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.registerStudent1, 4);
+            this.registerStudent1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.registerStudent1.Location = new System.Drawing.Point(3, 53);
+            this.registerStudent1.Name = "registerStudent1";
+            this.registerStudent1.Size = new System.Drawing.Size(617, 452);
+            this.registerStudent1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 508);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sistema de inscripcion";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Ejercicio_01.RegisterStudent registerStudent1;
+        private System.Windows.Forms.Button btnConsult;
+        private System.Windows.Forms.Button btnInscription;
+        private System.Windows.Forms.Button btnSignature;
+        private System.Windows.Forms.Button btnStudent;
+    }
+}
